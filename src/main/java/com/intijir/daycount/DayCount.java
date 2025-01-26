4e8ea124ce3b17fc80ef306f7ba5236c957b129a
Pete MC-Mods <mcmods.pete@gmail.com>
2025-01-26 22:56:51 +0100

    move stuff to client file

--- src/main/java/com/intijir/daycount/DayCount.java
+++ src/main/java/com/intijir/daycount/DayCount.java
@@ -2,53 +2,16 @@ package com.intijir.daycount;
 
 import com.intijir.daycount.config.DayCountConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.util.math.MatrixStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DayCount implements ModInitializer, HudRenderCallback {
+public class DayCount implements ModInitializer {
     public static final String MOD_ID = "daycount";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-    private int cachedDayTime = -1;
-
 
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing Day Count Mod");
         DayCountConfig.init();
-
-        ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                int currentDayTime = (int) (MinecraftClient.getInstance().world.getTimeOfDay() / 24000L);
-                if (currentDayTime != cachedDayTime) {
-                    // Day time has changed, update the cached value and do any necessary updates
-                    cachedDayTime = currentDayTime;
-                    // Update the GUI or other elements as needed
-                    MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
-                    HudRenderCallback.EVENT.register(this);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onHudRender(DrawContext drawContext, float tickDelta) {
-        if (DayCountConfig.INSTANCE.dayCountEnabled) {
-            int currentDay = (int) (MinecraftClient.getInstance().world.getTimeOfDay() / 24000L);
-            TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-            MatrixStack matrixStack = drawContext.getMatrices();
-
-            matrixStack.push();
-            matrixStack.translate(DayCountConfig.INSTANCE.locationX, DayCountConfig.INSTANCE.locationY, 0);
-            matrixStack.scale(DayCountConfig.INSTANCE.sizeX, DayCountConfig.INSTANCE.sizeY, 2.5f);
-            drawContext.drawTextWithShadow(textRenderer, "Day: " + (currentDay + DayCountConfig.INSTANCE.dayOffset), 2, 2, DayCountConfig.INSTANCE.color);
-            matrixStack.pop();
-        }
     }
 }
--- src/main/java/com/intijir/daycount/client/DayCountClient.java
+++ src/main/java/com/intijir/daycount/client/DayCountClient.java
@@ -1,13 +1,50 @@
 package com.intijir.daycount.client;
 
+import com.intijir.daycount.config.DayCountConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 
 @Environment(EnvType.CLIENT)
-public class DayCountClient implements ClientModInitializer {
+public class DayCountClient implements ClientModInitializer, HudRenderCallback {
+    private int cachedDayTime = -1;
+
     @Override
     public void onInitializeClient() {
         KeyInputHandler.register();
+
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+            if (client.player != null) {
+                int currentDayTime = (int) (MinecraftClient.getInstance().world.getTimeOfDay() / 24000L);
+                if (currentDayTime != cachedDayTime) {
+                    // Day time has changed, update the cached value and do any necessary updates
+                    cachedDayTime = currentDayTime;
+                    // Update the GUI or other elements as needed
+                    MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+                    HudRenderCallback.EVENT.register(this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onHudRender(DrawContext drawContext, float tickDelta) {
+        if (DayCountConfig.INSTANCE.dayCountEnabled) {
+            int currentDay = (int) (MinecraftClient.getInstance().world.getTimeOfDay() / 24000L);
+            TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+            MatrixStack matrixStack = drawContext.getMatrices();
+
+            matrixStack.push();
+            matrixStack.translate(DayCountConfig.INSTANCE.locationX, DayCountConfig.INSTANCE.locationY, 0);
+            matrixStack.scale(DayCountConfig.INSTANCE.sizeX, DayCountConfig.INSTANCE.sizeY, 2.5f);
+            drawContext.drawTextWithShadow(textRenderer, "Day: " + (currentDay + DayCountConfig.INSTANCE.dayOffset), 2, 2, DayCountConfig.INSTANCE.color);
+            matrixStack.pop();
+        }
     }
 }
