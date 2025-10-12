783b01395f4da71c7810f2db8bbf5ad6ed604589
Pete MC-Mods <mcmods.pete@gmail.com>
2025-10-12 10:47:28 +0200

    port to 1.21.9

--- src/client/java/com/intijir/daycount/DayCountClient.java
+++ src/client/java/com/intijir/daycount/DayCountClient.java
@@ -1,17 +1,14 @@
-package com.intijir.daycount.client;
+package com.intijir.daycount;
 
 import com.intijir.daycount.config.DayCountConfig;
+import com.intijir.daycount.handler.KeyInputHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gl.RenderPipelines;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
 import org.joml.Matrix3x2fStack;
 
@@ -38,12 +35,14 @@ public class DayCountClient implements ClientModInitializer {
         });
 
         HudElementRegistry.addLast(dayCountHudElement, (context, tCounter) -> {
-            Matrix3x2fStack matrixStack = context.getMatrices();
-            matrixStack.pushMatrix();
-            matrixStack.translate(DayCountConfig.INSTANCE.locationX, DayCountConfig.INSTANCE.locationY, matrixStack);
-            matrixStack.scale(DayCountConfig.INSTANCE.sizeX, DayCountConfig.INSTANCE.sizeY, matrixStack);
-            context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, DayCountConfig.INSTANCE.dayCounterString + (currentDay + DayCountConfig.INSTANCE.dayOffset), 2, 2, !MinecraftClient.getInstance().player.isSpectator() ? (int) Long.parseLong(DayCountConfig.INSTANCE.colorWithTransparency, 16) : 0);
-            matrixStack.popMatrix();
+            if (DayCountConfig.INSTANCE.dayCountEnabled) {
+                Matrix3x2fStack matrixStack = context.getMatrices();
+                matrixStack.pushMatrix();
+                matrixStack.translate(DayCountConfig.INSTANCE.locationX, DayCountConfig.INSTANCE.locationY, matrixStack);
+                matrixStack.scale(DayCountConfig.INSTANCE.sizeX, DayCountConfig.INSTANCE.sizeY, matrixStack);
+                context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, DayCountConfig.INSTANCE.dayCounterString + (currentDay + DayCountConfig.INSTANCE.dayOffset), 2, 2, !MinecraftClient.getInstance().player.isSpectator() ? (int) Long.parseLong(DayCountConfig.INSTANCE.colorWithTransparency, 16) : 0);
+                matrixStack.popMatrix();
+            }
         });
 
     }
--- src/client/java/com/intijir/daycount/handler/KeyInputHandler.java
+++ src/client/java/com/intijir/daycount/handler/KeyInputHandler.java
@@ -1,14 +1,16 @@
-package com.intijir.daycount.client;
+package com.intijir.daycount.handler;
 
+import com.intijir.daycount.DayCount;
 import com.intijir.daycount.config.DayCountConfig;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
-    public static final String KEY_CATEGORY_DAY_COUNT = "key.category.day_count_keybind";
+    public static final String KEY_CATEGORY_DAY_COUNT = "day_count_keybind";
     public static final String KEY_DAY_COUNT = "key.daycount.day_count";
 
     public static KeyBinding dayCountKey;
@@ -30,7 +32,7 @@ public class KeyInputHandler {
     }
 
     public static void register(){
-        dayCountKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(KEY_DAY_COUNT, InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, KEY_CATEGORY_DAY_COUNT));
+        dayCountKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(KEY_DAY_COUNT, InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, new KeyBinding.Category(Identifier.of(KEY_CATEGORY_DAY_COUNT))));
         registerKeyInputs();
     }
 }
